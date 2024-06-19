dd67b930f5bf089402d7effc10ff3a77729ac814
samnejad92 <m.samnejad92@gmail.com>
2024-06-19 10:53:51 +0330

    url changed

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
 function onLoad(){
     console.log('salam');
     $.ajax({
-        url:'http://unique-game.ir/api/ScoreController.php?action=read',
+        url:'https://reqres.in/api/users?page=2',
         error: function(){
             alert("something wrong");
         },
         success : function(result, status, xhr){
-            const tbody=document.querySelector('tbody');
-            console.log(typeof JSON.parse(result));
-            tbody.innerHTML=JSON.parse(result).map(item=>{
-                return `<tr>
-                <td>${item.id}</td>
-                <td>${item.nickName}</td>
-                <td>${item.score}</td>
-                <td>${item.mistake}</td>
-                <td>${item.help}</td>
-                <td>${item.time}</td>
-                </tr>`;
-            }).join('');
+            // const tbody=document.querySelector('tbody');
+            // console.log(typeof JSON.parse(result));
+            // tbody.innerHTML=JSON.parse(result).map(item=>{
+            //     return `<tr>
+            //     <td>${item.id}</td>
+            //     <td>${item.nickName}</td>
+            //     <td>${item.score}</td>
+            //     <td>${item.mistake}</td>
+            //     <td>${item.help}</td>
+            //     <td>${item.time}</td>
+            //     </tr>`;
+            // }).join('');
+            console.log(result);
+            console.log(status);
+            console.log(xhr);
+            
         },
     });
 }
